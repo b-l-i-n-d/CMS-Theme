f22783b5e8aaacb1501fe8f53e27bc0b35fd5de4
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-11-07 10:30:50 +0600

    Removed hover effect of featured section

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,15 +15,15 @@ module.exports = {
 
             width: {
                "1/2-4": "calc(50% - 16px)",
-               "1/3-4": "calc(33% - 16px)",
-               "2/3-4": "calc(66% - 16px)",
+               "1/3-4": "calc(33.3333% - 16px)",
+               "2/5-4": "calc(40% - 16px)",
+               "3/5-4": "calc(60% - 16px)"
             }
         },
     },
     variants: {
         extend: {
             display: ['group-hover'],
-            width: ['hover']
         },
     },
     plugins: [],
