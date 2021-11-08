0c3bedc41be4f9f0705f7e29b42b0874cde0fee0
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-11-08 21:45:41 +0600

    Content slider

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,22 +1,43 @@
 $(document).ready(() => {
     $(".owl-carousel").owlCarousel();
 
-    $('.carouselPrev').eq(0).click(() => {
-        $('.owl-carousel').eq(0).trigger('prev.owl.carousel');
+    $('.trendingPrev').click(() => {
+        $('.trending-carousel').trigger('prev.owl.carousel');
     });
-    $('.carouselNext').eq(0).click(() => {
-        $('.owl-carousel').eq(0).trigger('next.owl.carousel');
+    $('.trendingNext').click(() => {
+        $('.trending-carousel').trigger('next.owl.carousel');
     });
 
-    $('.carouselPrev').eq(1).click(() => {
-        $('.owl-carousel').eq(1).trigger('prev.owl.carousel');
+    $('.memberPrev').click(() => {
+        $('.member-carousel').trigger('prev.owl.carousel');
     });
-    $('.carouselNext').eq(1).click(() => {
-        $('.owl-carousel').eq(1).trigger('next.owl.carousel');
+    $('.memberNext').click(() => {
+        $('.member-carousel').trigger('next.owl.carousel');
     });
 });
 
-$(".owl-carousel").owlCarousel({
+$(".trending-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        640: {
+            items: 2,
+        },
+        768: {
+            items:3,
+        },
+        1024: {
+            items: 4,
+        },
+    },
+});
+
+$(".member-carousel").owlCarousel({
     loop: true,
     margin: 20,
     nav: false,
