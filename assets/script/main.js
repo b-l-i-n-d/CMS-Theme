d26dab1a0b701095d7b913327ac714c3390e7324
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-12-21 11:10:26 +0600

    Content slider

---
+++ assets/script/main.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        type: 'bullets',
+        clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    // Autoplay
+    autoplay: {
+        delay: 2000,
+    },
+});
